41cb6f830fe57200bf07ff11ce4ec0a541728675
Colm Britton <colmjude@gmail.com>
2019-08-15 09:28:19 +0100

    Make it easier to use 'flask shell'

--- application/wsgi.py
+++ application/wsgi.py
@@ -1,8 +1,10 @@
 import os
 from application.factory import create_app
+from application.extensions import db
+from application.models import CompulsoryPurchaseOrder
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'config.DevelopmentConfig')
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db, CPO=CompulsoryPurchaseOrder)
