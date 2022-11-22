b98ec6798a824d2eaea1963e68ded631c7af2a3a
Adam Shimali <adam.shimali@gmail.com>
2022-11-22 09:36:17 +0000

    fix postgres url in config for heroku

--- config/config.py
+++ config/config.py
@@ -14,8 +14,11 @@ class Config(object):
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_ROOT, os.pardir))
     SECRET_KEY = os.getenv('SECRET_KEY')
     SECRET_KEY = os.getenv('SECRET_KEY')
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID')
     AUTH0_CLIENT_SECRET = os.getenv('AUTH0_CLIENT_SECRET')
     AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
